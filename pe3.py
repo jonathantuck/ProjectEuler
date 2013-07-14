bb0d91ab7e29db01b4236f54efcee96b8e4197e7
Jonathan Tuck <jonathantuck1@gmail.com>
2013-07-14 16:37:57 +0200

    13July2013

--- pe3.py
+++ pe3.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 def isPrime(n):
     for i in range(2, int(n**0.5)+1):
@@ -6,15 +7,21 @@ def isPrime(n):
             return False
     return True
 
+start = time.time()
 
 x = 600851475143
 
 primelist = []
-n = 2
+n = 1
+
 while x != 1:
-    for i in range(2,x):
-        if isPrime(i) and x%i==0:
-            primelist.append(i)
-            x = x/i
-print primelist
-    
+    n += 1
+    if x%n == 0 and isPrime(n):
+        primelist.append(n)
+        x = x/n
+        n = 1
+print max(primelist)
+
+stop = time.time()
+
+print stop-start
